3b639c86808bbf012da3d8be37720cdcc5bb4d4a
Eliane Röösli <22400255+elianeroosli@users.noreply.github.com>
2021-02-16 08:55:59 +0100

    Update channel_wise_lstms.py

--- models/keras_models/channel_wise_lstms.py
+++ models/keras_models/channel_wise_lstms.py
@@ -11,17 +11,6 @@ from keras.layers.merge import Concatenate
 
 from models.keras_utils import Slice, LastTimestep, ExtendMask
 
-### BIDIRECTIONAL:
-# In problems where all timesteps of the input sequence are available, Bidirectional LSTMs 
-# train two instead of one LSTMs on the input sequence. The first on the input sequence as-is 
-# and the second on a reversed copy of the input sequence. This can provide additional context 
-# to the network and result in faster and even fuller learning on the problem.
-
-### Recurrent dropout:
-# masks connections between the recurrent units
-
-
-### for further info: https://keras.io/layers/writing-your-own-keras-layers/
 
 class Network(Model):
 
@@ -184,8 +173,7 @@ class Network(Model):
         # (11) build the specified network in keras
         super(Network, self).__init__(inputs=inputs, outputs=outputs)
 
-        
-        
+       
         
     # print characteristics of network
     def say_name(self):
